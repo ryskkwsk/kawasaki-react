e3db2645628d50d584e53913f9c18ca895c044e3
川崎 亮輔 <kawasakiryosuke@team-lab.com>
2020-01-20 00:48:08 +0900

    [update]・App.jsの修正

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import * as authAction from "./actions/authAction";
+
+import Auth from "./Auth";
+import Token from "./Token";
+
+import LoginPage from "./components/pages/LoginPage.js";
+import ItemListPage from "./components/pages/ItemListPage.js";
+
+/**
+ * Reduxのstateをpropsに展開する
+ */
+function mapStateToProps(state) {
+  return {
+    auth: state.auth
+  };
+}
+
+/**
+ * Reduxのactionをpropsに展開する
+ */
+function mapDispatchToProps(dispatch) {
+  return {
+    authAction: bindActionCreators(authAction, dispatch)
+  };
+}
+
+/**
+ * 認証とルーティングを行う
+ */
+class App extends React.Component {
+  render = () => (
+    <div>
+      <Switch>
+        {/* 認証が不要なルーティング */}
+        <Route exact path={"/login"} component={LoginPage} />
+        <Route path={"/token/:token"} component={Token} />
+        {/* 認証が必要なルーティング */}
+        <Auth isLogin={this.props.auth.isLogin}>
+          <Switch>
+            <Route exact path={"/"} component={ItemListPage} />
+            <Route exact path={"/items"} component={ItemListPage} />
+            <Route exact path={"/items/search"} component={ItemListPage} />
+          </Switch>
+        </Auth>
+      </Switch>
     </div>
   );
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
