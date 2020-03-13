7b62d384453e0d13ac6d8eafeeae14be650867e8
RyosukeKawasaki <ryooosuke1111@gmail.com>
2020-03-13 18:36:45 +0900

    [change]・react-cookieではなく、js-cookieでのトークンの管理に変更

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,26 +1,24 @@
 import actionType from "../config/actionType";
-import { Cookies } from 'react-cookie';
+import Cookies from 'js-cookie';
 
 const TOKEN_KEY = "accessToken";
-const cookies = new Cookies();
 const initialAuthState = {
-  accessToken: cookies.get(TOKEN_KEY),
-  isLogin: cookies.get(TOKEN_KEY) !== null
+  accessToken: Cookies.get(TOKEN_KEY),
+  isLogin: Cookies.get(TOKEN_KEY) !== null
 };
 
 const reducer = (state = initialAuthState, action) => {
   switch (action.type) {
     case actionType.LOGIN:
-      cookies.set(TOKEN_KEY, action.payload, {
-        maxAge: 3600, // ログイン後1時間でクッキーが切れます
-      });
+      Cookies.set(TOKEN_KEY, action.payload, { expires: 60/1440 } // ログイン後1時間でクッキーが切れます
+      );
       return {
         ...state,
         accessToken: action.payload,
         isLogin: true
       };
     case actionType.LOGOUT:
-      cookies.remove(TOKEN_KEY);
+      Cookies.remove(TOKEN_KEY);
       return {
         ...state,
         accessToken: null,
